680e4444e3f69a7f4ccc0d5725c76ed1514fa534
GustaBarbosaa <127910329+GustaBarbosaa@users.noreply.github.com>
2024-09-18 19:45:44 -0300

    Section Produto - Gustavo

---
+++ TelaEficaz/script.js
@@ -0,0 +1,7 @@
+document.getElementById("botaoAzul").addEventListener("click", function () {
+    document.getElementById("imagemProduto").src = "img/produto1.png";
+});
+
+document.getElementById("botaoVermelho").addEventListener("click", function () {
+    document.getElementById("imagemProduto").src = "img/produto2.png";
+});
